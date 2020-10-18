64e96ba070a9c494ba2595b76ed1c14f9d8c6eb3
sarthakchaudhary13 <sarthakchaudhary13@gmail.com>
2020-10-19 02:53:56 +0530

    getting sentence from fastapi uri

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main (){
-    
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let sentence_uri = "https://typer-text.herokuapp.com/type-text/";
+
+    let resp = reqwest::get(sentence_uri)
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+
+    println!("{:#?}", resp.get("message").unwrap());
+    Ok(())
 }
